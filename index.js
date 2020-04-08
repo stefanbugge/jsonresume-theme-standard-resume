cc0266a60bc4d8fe44496d07dfaa0b7500997884
Stefan Loeschcke <stefan@loeschcke.dk>
2020-04-08 20:47:58 +0200

    adjusting theme layout and adding profiles and languages sections

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var fs = require('fs');
 var path = require('path');
 var Handlebars = require('handlebars');
+var helperDate = require('helper-date');
+
+function normalizeNetworkName (network) {
+  return (network || '').toLowerCase().replace(/\s/g, '-')
+}
 
 function render(resume) {
   var css = fs.readFileSync(__dirname + '/style.css', 'utf-8');
   var tpl = fs.readFileSync(__dirname + '/resume.hbs', 'utf-8');
   var partialsDir = path.join(__dirname, 'partials');
   var filenames = fs.readdirSync(partialsDir);
-  Handlebars.registerHelper('date', require('helper-date'));
+
+  Handlebars.registerHelper('date', helperDate);
+  Handlebars.registerHelper('normalizeNetworkName', normalizeNetworkName)
 
   filenames.forEach(function(filename) {
     var matches = /^([^.]+).hbs$/.exec(filename);
